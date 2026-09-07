9a469ae7f879a5c1bdcc7c1368a2fa8cbabf2ccb
F_RH <165563333+FadlyRH@users.noreply.github.com>
2026-09-07 22:25:42 +0700

    menambahkan div pada html dan menambahkan file js dan merubah style pada css

---
+++ script.js
@@ -0,0 +1,28 @@
+function tampilkanjam(){
+    const sekarang = new Date();
+    
+    //mengambil jam, menit dan detik dari objek date
+    const jam = String(sekarang.getHours()).padStart(2, "0");
+    const menit = String(sekarang.getMinutes()).padStart(2, "0");
+    const detik = String(sekarang.getSeconds()).padStart(2, "0");
+
+    //menampilkan jam, menit dan detik
+    document.getElementById("jam").textContent = `${jam}:${menit}:${detik}`;
+
+    //mengambil tanggal, bulan dan tahun dari object date
+    const tanggal = sekarang.toLocaleDateString("id-ID", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    });
+
+    //menampilkan tanggal, bulan dan tahun
+    document.getElementById("tanggal").textContent = tanggal;
+}
+
+//jalannkan fungsi fungsi tampilkanjam setiap 1 detik
+setInterval(tampilkanjam, 1000);
+
+//jalankan langsung saat halaman dimuat
+tampilkanjam();
